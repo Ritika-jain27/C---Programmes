c892a07a60e314d19f5a0ec0fbf970e532e80c13
Ritika-jain27 <ritikaj088@gmail.com>
2022-03-06 21:27:24 +0530

    Hollow Rectangle Pattern

---
+++ Pattern Questions/Hollow_Rectangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int row, col, i, j;
+    cout << "Enter Number of Rows & coloumns in rectangle : ";
+    cin >> row >> col;
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < col; j++)
+        {
+            if (i == 0 || i == row - 1 || j == 0 || j == col - 1)
+                cout << "*";
+
+            else
+                cout << " ";
+        }
+        cout << endl;
+    }
+}
