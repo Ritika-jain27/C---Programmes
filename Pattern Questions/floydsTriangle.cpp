a4705968a6d12abefba021ad2644ce8c4884bcf7
Ritika-jain27 <ritikaj088@gmail.com>
2022-03-06 22:52:17 +0530

    Patterns

--- Pattern Questions/floydsTriangle.cpp
+++ Pattern Questions/floydsTriangle.cpp
@@ -4,14 +4,15 @@ using namespace std;
 int main()
 {
     int n;
-    int i, j;
+    int i, j,count=1;
     cout << " Enter number of number : ";
     cin >> n ;
     for (i = 1; i <=n; i++)
     {
-        for (j = 0; j <i ; j++)
+        for (j = 1; j <=i ; j++)
         {
-            cout<<i;
+            cout<<count<<" ";
+            count++;
         }
         cout<<endl;
     }
