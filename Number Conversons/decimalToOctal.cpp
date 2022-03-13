90f13624afc6603ec7e0cd115820a859ab055eb9
Ritika-jain27 <ritikaj088@gmail.com>
2022-03-13 19:09:47 +0530

    Decimal to octal

---
+++ Number Conversons/decimalToOctal.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int deciToOctal(int n)
+{
+    int x = 1, ans = 0;
+    while (x <= n)
+    {
+        x *= 8;
+    }
+    x /= 8;
+    while (x > 0)
+    {
+        int lastdigit = n / x;
+        n -= lastdigit * x;
+        x /= 8;
+        ans = ans * 10 + lastdigit;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n;
+    cout << "Enter any Decimal Number : ";
+    cin >> n;
+    cout << deciToOctal(n) << endl;
+
+    return 0;
+}
